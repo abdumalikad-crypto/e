406492f403eb7efdacbcb19ea28480ee2af6355b
abuse1221 <abdumalikabdullaev35@gmail.com>
2026-06-22 01:55:20 +0500

    added js

---
+++ apps.js/index.js
@@ -0,0 +1,12 @@
+let person = confirm('do you want a see');
+if (person === true) {
+  console.log('Marhamat');
+}else{
+  console.log('marhamat pls');
+}
+let massege = prompt ("kechikib ketti");
+if (massege === 'mayli','xa') {
+  console.log('accepted');
+}else if (massege === "yoq") {
+  console.log('not accepted');
+}
